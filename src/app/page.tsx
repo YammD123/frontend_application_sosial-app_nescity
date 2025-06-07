dbf3ba1b145a7c90cafa88e63249e43bf54bcebe
YammD123 <siamqueen52@gmail.com>
2025-06-07 15:43:00 +0700

    memperbaiki peralihan pada pada root layout dengan pengguna yang sudah login dan belum login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,20 @@
 import { userLoaderStatus } from "@/loaders/user-loader";
 import { redirect } from "next/navigation";
 
-export default function Home() {
+export  default async function Home() {
   // const [state, formAction] = useActionState(loginAction, {
   //   message: "",
   //   success: false,
   //   fieldError: {},
   // });
 
-  const auth = userLoaderStatus()
-  if(!auth) redirect('/login')
+  const auth = await userLoaderStatus()
+  if(!auth){
+    redirect("/login")
+  }
+  else{
+    redirect("/content/beranda/home")
+  }
   return(
     <>
     </>
