4ea8d57569a921658b88d62f9ad6b57ac72537ed
YammD123 <siamqueen52@gmail.com>
2025-06-08 15:53:11 +0700

    menyelesaikan bagian action pada createPost

--- src/actions/post-action.ts
+++ src/actions/post-action.ts
@@ -1,6 +1,32 @@
-export const postActionCreate = async(prev:any,formData:FormData)=>{
-    const caption = formData.append("caption",formData.get("caption")!)
-    const media = formData.append("image",formData.get("image")!);
+"use server"
 
-    
+import { BASE_URL } from "@/lib/base-url";
+import { cookies } from "next/headers";
+
+export const postActionCreate = async(prev:any,formData:FormData)=>{
+    const cookieStore = await cookies()
+    const cookie = cookieStore.get("token")?.value
+    if(!cookie){
+        return null
+    }
+
+    const form = new FormData()
+    form.append("caption",formData.get("caption")!)
+    const media = formData.get("media")
+    if(media && (media as File).size > 0){
+        form.append("media",media)
+    }
+
+    const res = await fetch(`${BASE_URL}/post`,{
+        method:"POST",
+        headers: {
+            cookie: `token=${cookie}`
+        },
+        body: form
+    })
+    const data = await res.json()
+    return {
+        message: "berhasil membuat post",
+        success: true
+    }
 }
--- src/components/header-home.tsx
+++ src/components/header-home.tsx
@@ -1,15 +1,14 @@
 "use client";
 
 import { Card, CardContent } from "@/common/shadcn/card";
-import { Dialog, DialogContent } from "@/common/shadcn/dialog";
+import { Dialog, DialogContent, DialogTitle, DialogTrigger } from "@/common/shadcn/dialog";
 import { Separator } from "@/common/shadcn/separator";
-import { DialogTitle, DialogTrigger } from "@radix-ui/react-dialog";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useRef } from "react";
-import { CldUploadWidget } from "next-cloudinary";
-import { Images } from "lucide-react";
+import React, { useActionState, useRef } from "react";
+import { Images, Trash2 } from "lucide-react";
 import { Button } from "@/common/shadcn/button";
+import { postActionCreate } from "@/actions/post-action";
 
 interface HomeProps {
   profile: {
@@ -22,18 +21,46 @@ interface HomeProps {
 export default function Headerhome({ profile }: HomeProps) {
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
   const inputRef = useRef<HTMLInputElement>(null);
+  const [preview, setPreview] = React.useState<string[]>([]);
+  const [state,formData] = useActionState(postActionCreate,{message:"",success:false});
+
+  //mengatur tinggi textarea agar tetap sesuai dengan isi
   const handleInput = () => {
     if (textAreaRef.current) {
       textAreaRef.current.style.height = "auto";
-      textAreaRef.current.style.height = textAreaRef.current.scrollHeight + "px";
+      textAreaRef.current.style.height =
+        textAreaRef.current.scrollHeight + "px";
     }
-  }
+  };
 
-  const handleClick =() => {
+  //trigger file input pada icon image yg di click
+  const handleClick = () => {
     if (inputRef.current) {
       inputRef.current.click();
     }
-  }
+  };
+
+  //mengambil gambar dan menampilkan pada preview
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (!files) {
+      return;
+    }
+    const urls = Array.from(files).map((file) => URL.createObjectURL(file));
+    setPreview(urls);
+  };
+
+
+  //menghapus gambar pada preview
+  const handleRemove = (index: number) => {
+    const updatedUrls = [...preview];
+    updatedUrls.splice(index, 1);
+    setPreview(updatedUrls);
+
+    if (updatedUrls.length === 0 && inputRef.current) {
+      inputRef.current!.value = "";
+    }
+  };
 
   return (
     <>
@@ -61,34 +88,62 @@ export default function Headerhome({ profile }: HomeProps) {
                   </DialogTitle>
                   <Separator />
                   <div className="flex gap-2 items-center">
-                    <Image
-                      src={profile.avatar_image}
-                      alt={profile.name}
-                      width={40}
-                      height={40}
-                      className="rounded-full"
-                    />
+                    {profile.avatar_image && (
+                      <Image
+                        src={profile.avatar_image}
+                        alt={profile.name}
+                        width={40}
+                        height={40}
+                        className="rounded-full"
+                      />
+                    )}
                     <h1>{profile.name}</h1>
                   </div>
                   <div>
-                    <form encType="" action="">
+                    <form  action={formData}>
                       <textarea
                         className="w-full resize-none focus:outline-none p-2 rounded-lg"
                         name="caption"
                         ref={textAreaRef}
                         onInput={handleInput}
-                        id=""
                         placeholder="Tulis sesuatu disini..."
                       ></textarea>
+                      <div className="flex flex-col gap-1">
+                        {preview.map((url, index) => (
+                          <div className="relative" key={url}>
+                            <Image 
+                            src={url} 
+                            alt="" 
+                            width={50} 
+                            height={50} 
+                            className="w-full h-30 object-cover"
+                            />
+                            <Button 
+                            variant={"destructive"} 
+                            className="absolute top-1 right-1 z-10"
+                            onClick={() => handleRemove(index)}>
+                              <Trash2
+                              size={20}
+                              />
+                            </Button>
+                          </div>
+                        ))}
+                      </div>
                       <Separator />
                       <div className="flex justify-end p-4">
                         <div onClick={handleClick}>
-                          <Images 
-                          size={25}
-                          />
+                          <Images size={25} />
                         </div>
-                          <input ref={inputRef} type="file" multiple className="hidden" />
+                        <input
+                          ref={inputRef}
+                          type="file"
+                          multiple
+                          name="media"
+                          className="hidden"
+                          onChange={handleChange}
+                        />
                       </div>
+                        <Button variant={"outline"} type="submit">Posting</Button>
                     </form>
                   </div>
                 </DialogContent>
